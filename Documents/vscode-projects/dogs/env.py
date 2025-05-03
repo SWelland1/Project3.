f68866d482c9903f682e92f39d0ceab595b6f526
SWelland1 <sarah.welland@hotmail.com>
2025-05-03 13:48:56 +0100

    Post Model add

---
+++ Documents/vscode-projects/dogs/env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_nUBHa47kRpbS@ep-royal-tooth-a26obpxr.eu-central-1.aws.neon.tech/quail_wifi_wad_190782")
+
+os.environ.setdefault("SECRET_KEY", "1986")
